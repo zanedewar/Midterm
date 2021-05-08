cb2da338bc3ae4157854aee5a626a534e62ec5e0
zanedewar <zane_dewar@hotmail.com>
2021-05-08 15:23:57 -0700

    Javascript
    Added javascript link to all html files and added background image

---
+++ Midtermjs.js
@@ -0,0 +1,10 @@
+var elTopMenu = document.getElementById('topmenu');
+elTopMenu.innerHTML = '<ul>' +
+				'<li><a href="index.html">Home</a></li>' +
+				'<li><a href="about.html">About Me</a></li>' +
+				'<li><a href="coursework.html">Coursework</a></li>' +
+				'<li><a href="#">Artifacts</a></li>' +
+				'<li><a href="#">Module Assignments</a></li>' +
+				'<li><a href="#">Contact Me</a></li>' +
+				'<li><a href="#">Interests</a><li>' +
+			'</ul>'
