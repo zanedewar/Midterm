9ba213ec21d0fa257135074fcd09dcda27a6298c
zanedewar <zane_dewar@hotmail.com>
2021-05-08 17:36:11 -0700

    Get it done

--- Midtermjs.js
+++ Midtermjs.js
@@ -1,10 +1,15 @@
-var elTopMenu = document.getElementById('topmenu');
-elTopMenu.innerHTML = '<ul>' +
-				'<li><a href="index.html">Home</a></li>' +
-				'<li><a href="about.html">About Me</a></li>' +
-				'<li><a href="coursework.html">Coursework</a></li>' +
-				'<li><a href="#">Artifacts</a></li>' +
-				'<li><a href="#">Module Assignments</a></li>' +
-				'<li><a href="#">Contact Me</a></li>' +
-				'<li><a href="#">Interests</a><li>' +
-			'</ul>'
+window.onload = function() {
+	console.log("hello");
+	menuFunction();
+};
+function menuFunction() {
+	document.getElementById("topmenu").innerHTML = `<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="about.html">About Me</a></li>
+				<li><a href="coursework.html">Coursework</a></li>
+				<li><a href="artifacts.html">Artifacts</a></li>
+				<li><a href="modass.html">Module Assignments</a></li>
+				<li><a href="contact.html">Contact Me</a></li>
+				<li><a href="interests.html">Interests</a><li>
+			</ul>`
+}
