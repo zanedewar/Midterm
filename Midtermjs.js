e6d11dc6d46e7c306303c1d63fec5a0e810a4736
zanedewar <zane_dewar@hotmail.com>
2021-06-07 00:11:47 -0700

    Giga update

--- Midtermjs.js
+++ Midtermjs.js
@@ -1,5 +1,4 @@
 window.onload = function() {
-	console.log("hello");
 	menuFunction();
 };
 function menuFunction() {
@@ -12,4 +11,30 @@ function menuFunction() {
 				<li><a href="contact.html">Contact Me</a></li>
 				<li><a href="interests.html">Interests</a><li>
 			</ul>`
+}
+function validateForm() {
+	var errorMessages = "";
+	var validName = validateName(document.getElementById("userName").value, 20);
+	var validEmail = validateEmail(document.getElementById("userEmail").value);
+	var validComments = validateComments(document.getElementById("userComments").value);
+	if(!validName)
+		errorMessages += "<p>Name is required and cannot be greater than 20 characters.</p>"
+	if(!validEmail)
+		errorMessages +="<p>Invalid email</p>"
+	if(!validComments)
+		errorMessages += "<p>Comments are required</p>"
+	document.getElementById("errorMessages").innerHTML = errorMessages;
+	console.log(errorMessages);
+	return errorMessages.length === 0;
+}
+function validateName(firstName, maxLength) {
+	return !(firstName === "null" || firstName ==="" || firstName.length > maxLength || !checkCharacters(firstName));
+}
+function validateEmail(email) {
+	var atpos = email.indexOf("@");
+	var dotpos = email.lastIndexOf(".");
+	return !(atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email.length);
+}
+function validateComments(comments) {
+	return comments.length > 0;
 }
